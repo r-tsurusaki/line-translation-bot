d5b6721b066f11707ab6f961d0962e196ca9cf83
r_tsurusaki <r_tsurusaki@172.31.13.53>
2018-10-27 18:15:14 +0900

    first commit

---
+++ src/main/java/com/gwa/GwaWebApplication.java
@@ -0,0 +1,12 @@
+package com.gwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwaWebApplication.class, args);
+	}
+}
